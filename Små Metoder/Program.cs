9936eaf8e1c2a9bc0c7ac427da6bcb815ce505e8
Morten Engemand <engemand@hotmail.com>
2018-05-15 13:53:34 +0200

    Små metoder

--- Små Metoder/Program.cs
+++ Små Metoder/Program.cs
@@ -8,8 +8,45 @@ namespace Små_Metoder
 {
     class Program
     {
+        static int LægSammen(int a, int b)
+        {
+            return (a + b);
+        }
+
+        static double BeregnAreal(double radius)
+        {
+            double areal = radius * radius * System.Math.PI;
+            return areal;            
+        }
+
+        static void udskriv(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+        static double gennemsnit(int[] månedsløn)
+        {
+            double snit = 0;
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                snit += månedsløn[i];
+            }
+
+            snit = snit / (månedsløn.Length);
+            return snit;
+        }
+
         static void Main(string[] args)
         {
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res);
+            double res2 = BeregnAreal(5);
+            Console.WriteLine(res2);
+            udskriv("Dette er en test");
+            int[] løn = { 10000, 5000, 30000 };
+            double gns = gennemsnit(løn);
+            Console.WriteLine(gns);
         }
     }
+
 }
